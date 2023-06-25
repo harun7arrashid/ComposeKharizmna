d6dd87c836eac3f7f500393c902b47d590159616
Harun AR-Rashid <harun4arrashid@gmail.com>
2023-06-25 10:59:14 +0700

    1

--- app/src/main/java/id/ackerman/composekharizma/MainActivity.kt
+++ app/src/main/java/id/ackerman/composekharizma/MainActivity.kt
@@ -3,41 +3,70 @@ package id.ackerman.composekharizma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.ackerman.composekharizma.ui.theme.ComposeKharizmaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeKharizmaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .border(width = 1.dp, color = Color.Red)
+                        .fillMaxWidth() // match_parent
+                        .height(200.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+
+                    Text(text = "Jetpack Compose Mantap 1")
+                    Text(
+                        text = "Text ke 2",
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+
+                }
+
+                Spacer(modifier = Modifier.padding(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .border(width = 1.dp, color = Color.Blue)
+                        .fillMaxWidth() // match_parent
+                        .height(200.dp),
+                    horizontalArrangement = Arrangement.Center // ngakalin gmna caranya center di Row
+                ) {
+
+                    Text(
+                        text = "Jetpack Compose Mantap 1",
+                        modifier = Modifier.align(
+                            Alignment.CenterVertically
+                        )
+                    )
+                    Text(
+                        text = "Mantap 2",
+                        modifier = Modifier.align(
+                            Alignment.CenterVertically
+                        )
+                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeKharizmaTheme {
-        Greeting("Android")
     }
 }
