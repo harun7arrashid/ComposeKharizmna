5ca31de091ed82d3c37ea9737eff444830f5dcda
Harun AR-Rashid <harun4arrashid@gmail.com>
2023-06-25 11:39:12 +0700

    3

--- app/src/main/java/id/ackerman/composekharizma/MainActivity.kt
+++ app/src/main/java/id/ackerman/composekharizma/MainActivity.kt
@@ -3,14 +3,17 @@ package id.ackerman.composekharizma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,51 +24,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Column(
-                modifier = Modifier.padding(16.dp)
+            // gda vertical dan horizontal, jd Box ini kek menimpa satu sama lain
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .fillMaxHeight(0.5f) // setengah dari layar
+                    .background(color = Color.Red)
+                    .padding(16.dp)
             ) {
 
-                Column(
+                Box(
                     modifier = Modifier
-                        .border(width = 1.dp, color = Color.Red)
-                        .fillMaxWidth() // match_parent
-                        .height(200.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-
-                    Text(text = "Jetpack Compose Mantap 1")
-                    Text(
-                        text = "Text ke 2",
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-
-                }
-
-                Spacer(modifier = Modifier.padding(16.dp))
-
-                Row(
-                    modifier = Modifier
-                        .border(width = 1.dp, color = Color.Blue)
-                        .fillMaxWidth() // match_parent
-                        .height(200.dp),
-                    horizontalArrangement = Arrangement.Center // ngakalin gmna caranya center di Row
-                ) {
-
-                    Text(
-                        text = "Jetpack Compose Mantap 1",
-                        modifier = Modifier.align(
-                            Alignment.CenterVertically
-                        )
-                    )
-                    Text(
-                        text = "Mantap 2",
-                        modifier = Modifier.align(
-                            Alignment.CenterVertically
-                        )
-                    )
-
-                }
+                        .size(200.dp)
+                        .background(color = Color.Yellow)
+                        .padding(16.dp)
+                        .border(2.dp, color = Color.Black)
+                        .padding(16.dp)
+                        .border(width = 2.dp, color = Color.Blue)
+                )
             }
         }
     }
