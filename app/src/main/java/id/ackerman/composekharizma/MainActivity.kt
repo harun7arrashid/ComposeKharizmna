a018dce479acd1602f2f4881fa160ae6d78338ae
Harun AR-Rashid <harun4arrashid@gmail.com>
2023-07-08 17:37:38 +0700

    6

--- app/src/main/java/id/ackerman/composekharizma/MainActivity.kt
+++ app/src/main/java/id/ackerman/composekharizma/MainActivity.kt
@@ -1,5 +1,6 @@
 package id.ackerman.composekharizma
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,17 +22,13 @@ import androidx.compose.ui.unit.dp
 import id.ackerman.composekharizma.ui.theme.ComposeKharizmaTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Column() {
-                
-                CustomButton(text = "Button Pertama", Color.Red)
-                CustomButton(text = "Button Kedua", Color.Green)
+            ScaffoldExample()
 
-            }
-            
         }
     }
 }
@@ -49,7 +48,65 @@ fun CustomButton(text: String, color: Color) {
             text = text,
             color = Color.Black
         )
+    }
+}
+
+@Composable
+fun CustomCheckBox(
+    title: String,
+    checkboxState: Boolean,
+    onCheckboxPressed: ((Boolean) -> Unit)?
+) {
+
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Checkbox(
+            checked = checkboxState,
+            onCheckedChange = onCheckboxPressed
+        )
+
+        Spacer(modifier = Modifier.padding(4.dp))
+        Text(text = title)
 
     }
-    
+
+}
+
+@Composable
+fun ScaffoldExample() {
+    val materialBlue700 = Color(0xff1976D2)  // kode color di compose itu diawali dgn 0xff
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Scaffold Tutorial") },
+                backgroundColor = materialBlue700
+            )
+        },
+
+        bottomBar = {
+
+            BottomAppBar(backgroundColor = materialBlue700) {
+
+                Text("Bottom bar")
+
+            }
+        },
+
+        floatingActionButton = {
+
+            FloatingActionButton(onClick = {}) {
+
+                Text("+")
+
+            }
+        },
+
+        floatingActionButtonPosition = FabPosition.End,
+        drawerContent = { Text(text = "Ini drawer") },
+        content = { Text(text = "Ini Konten") }
+
+    )
 }
